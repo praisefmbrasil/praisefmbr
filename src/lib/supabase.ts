7bed49a05ca6d3ae230ade1ea588ac84414bfe05
PRAISE FM BRASIL <praisefmbrasil@gmail.com>
2026-01-24 23:52:39 +0000

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://zhxickunddldjdusalfq.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_xFf78d86OQqGf8EkGpOqAQ_HIEmW92f';
+const SUPABASE_URL = 'https://ofgbifslafyuuilltwek.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_VpvkBirkYSrIBfDIUfLPXw_ZhcTvtRh';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
