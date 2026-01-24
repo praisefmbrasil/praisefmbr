f5b3902cff0d26f115bb2a00d7a2bd5145cd557f
PRAISE FM BRASIL <praisefmbrasil@gmail.com>
2026-01-24 01:11:19 +0000

    update

--- src/router.tsx
+++
@@ -1,8 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom'
-import AppHome from './pages/AppHome'
-import SiteHome from './pages/AppHomePage'  // Alterado para AppHomePage
-
-export const router = createBrowserRouter([
-  { path: '/', element: <SiteHome /> },
-  { path: '/app', element: <AppHome /> }
-])
