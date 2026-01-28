529d836eea71134c2bb932e4207a29e91deea97d
PRAISE FM BRASIL <praisefmbrasil@gmail.com>
2026-01-28 23:48:54 +0000

    update

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { Heart } from "lucide-react";
-import { FavoriteItem } from "../types";
+import type { FavoriteItem } from "../types";
 
 export interface Track {
   id: string;
