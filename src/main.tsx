7731e97f941df0066a7bf32f4d7e081d7dd02a7d
PRAISE FM BRASIL <praisefmbrasil@gmail.com>
2026-01-28 20:37:07 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
+import { AuthProvider } from './contexts/AuthContext';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Root element not found");
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
