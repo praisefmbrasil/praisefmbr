e935ba55dfa64df61946d7eb9323e6ea8321e00c
PRAISE FM BRASIL <praisefmbrasil@gmail.com>
2026-01-29 06:12:36 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Permite que o servidor seja acessado externamente no Codespaces
+    port: 5173, // Define a porta padrão
+    strictPort: true, // Garante que o Vite não tente mudar de porta se a 5173 estiver ocupada
+    hmr: {
+      clientPort: 443 // Necessário para o Hot Module Replacement funcionar via HTTPS no Codespaces
+    }
+  }
 })
