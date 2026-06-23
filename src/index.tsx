82224e7876e3eee80598bca17911444c9c16c262
PRAISE FM BRASIL <praisefmbrasil@gmail.com>
2026-06-23 00:26:58 +0000

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// @ts-ignore: Ignore missing type declarations for CSS imports in this project setup
 import './index.css'; // Importação crucial dos estilos global e Tailwind
 
 // Referência ao elemento root do seu index.html
