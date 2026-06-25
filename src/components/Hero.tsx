a8e6c4caa4e6f5edf529a28f55dc1d9d690d81d6
PRAISE FM BRASIL <praisefmbrasil@gmail.com>
2026-06-25 17:31:39 -0300

    Fix metadata display in Hero component
    Signed-off-by: PRAISE FM BRASIL <praisefmbrasil@gmail.com>

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -191,7 +191,7 @@ const Hero: React.FC<HeroProps> = ({
             </h2>
 
             <p className="text-lg text-gray-600 dark:text-gray-400 font-normal mb-6 max-w-xl">
-              {liveMetadata?.title ? `${liveMetadata.artist} - ${liveMetadata.title}` : currentProgram.host}
+              {liveMetadata?.host ? `${liveMetadata.host} - ${liveMetadata.host}` : currentProgram.host}
             </p>
 
             <button
